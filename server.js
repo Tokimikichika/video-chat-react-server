27c9ec2c8be940f416046a305f2deecd49407bce
Tokimikichika <ahileses@mail.ru>
2024-01-29 21:37:42 +0300

    add site client

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "*",
+		origin: "https://imaginative-figolla-cff577.netlify.app/",
 		methods: [ "GET", "POST" ]
 	}
 })
